24e94f7958428915409e1d0cf18836fcbac00c5c
John Reyes <john.jreye@gmail.com>
2019-11-18 00:38:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,7 +34,7 @@ def now_serving(array)
   if array.empty? == true
     puts "There is nobody waiting to be served!"
   elsif array.empty? == false
-    puts "Currently serving #{array.shift}"
+    puts "Currently serving #{array.shift}."
   end
 end
 
