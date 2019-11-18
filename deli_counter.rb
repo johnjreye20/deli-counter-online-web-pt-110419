432270f89141a310614b5fa129ca31202ff178ee
John Reyes <john.jreye@gmail.com>
2019-11-18 00:37:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts "Welcome #{name}. You are number #{array.index(name) +1} in line."
+  puts "Welcome, #{name}. You are number #{array.index(name) +1} in line."
   return name, position
 end
 
