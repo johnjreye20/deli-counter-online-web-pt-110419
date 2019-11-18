43da543b860915f091914ab23d9e165e2ffbe34f
John Reyes <john.jreye@gmail.com>
2019-11-18 00:34:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    message = "The line is currently:"
+    message = "The line is currently: "
 
     array.each_with_index do |
       value, index|
